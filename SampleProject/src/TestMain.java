70fbc8fbbe3c15ff92a8b627f42c042f8d76359d
neo073 <neo073@naver.com>
2018-06-17 00:59:45 +0900

    add main class

---
+++ SampleProject/src/TestMain.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author neo073
+ *
+ */
+public class TestMain {
+
+	/**
+	 * 
+	 */
+	public TestMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
